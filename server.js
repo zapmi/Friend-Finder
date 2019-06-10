6eaeccb0957c3ee7690551ba70cde5d0dd64bff1
Mike Z <mzapata2991@gmail.com>
2019-06-09 19:20:05 -0500

    add heroku links to console log, changed survey title

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('app/public'))
 
-app.listen(process.env.PORT || 8080, () => console.log('ALl is good'));
+app.listen(process.env.PORT || 8080, () => console.log('ALl is good: https://mighty-cove-17824.herokuapp.com/home.html \n https://mighty-cove-17824.herokuapp.com/survey.html'));
