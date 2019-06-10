97b6e284394eba2ef491130012e3916b1627ca49
Mike Z <mzapata2991@gmail.com>
2019-06-09 22:56:38 -0500

    made changes to html routes

--- app/routing/htmlRoutes.js
+++
@@ -1,7 +0,0 @@
-app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
