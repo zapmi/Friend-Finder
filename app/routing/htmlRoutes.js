128ddba4e50e05024ad4d17586d297c7713dfa55
Mike Z <mzapata2991@gmail.com>
2019-06-09 20:06:14 -0500

    added html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
